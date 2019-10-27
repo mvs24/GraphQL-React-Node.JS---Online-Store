68a15ce0c9cc01a178d27e4fbb70615d3382a8e0
mvs24 <40918003+mvs24@users.noreply.github.com>
2019-10-27 10:12:29 +0100

    single item query added

--- frontend/components/UpdateItem.js
+++ frontend/components/UpdateItem.js
@@ -16,6 +16,7 @@ const SINGLE_ITEM_QUERY = gql`
     }
   }
 `;
+
 const UPDATE_ITEM_MUTATION = gql`
   mutation UPDATE_ITEM_MUTATION($id: ID!, $title: String, $description: String, $price: Int) {
     updateItem(id: $id, title: $title, description: $description, price: $price) {
